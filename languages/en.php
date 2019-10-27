5a9d9a98e194e6ff991865824af8f99b492acaee
Dennis Ploeger <develop@dieploegers.de>
2019-10-27 12:40:17 +0100

    fix: Fixed settings and added missing language file

---
+++ languages/en.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'filetransport:settings:path' => 'Mail file path',
+    'filetransport:settings:path:help' => 'Path to where all sent out mails are stored as files'
+];
--- views/default/plugins/filetransport/settings.php
+++ views/default/plugins/filetransport/settings.php
@@ -1,8 +1,9 @@
 <?php
 
 echo elgg_view_field([
-    label => elgg_echo('filetransport:settings:path'),
-    help => elgg_echo('filetransport:settings:path:help'),
-    type => 'text',
-    name => 'params[path]'
+    '#label' => elgg_echo('filetransport:settings:path'),
+    '#help' => elgg_echo('filetransport:settings:path:help'),
+    '#type' => 'text',
+    name => 'params[path]',
+    value => elgg_get_plugin_setting('path', 'filetransport', elgg_get_data_path() . "/notifications_log/zend")
 ]);
